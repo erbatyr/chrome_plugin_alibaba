5a2b0d6af02768147d6d14aafce0ccdac1bcf2ec
erbatyr <erbatyr.akyn.97@gmail.com>
2021-06-11 18:08:06 +0600

    adding to git

--- getDataFromDOM.js
+++ getDataFromDOM.js
@@ -9,15 +9,6 @@ $("[type='submit']").click(function (){
             users.password = data[i].value
         }
     }
-    // chrome.storage.sync.get(["lenData"], function (result) {
-    //     let lenData = result.lenData;
-    //     console.log(typeof lenData);
-    //     chrome.storage.sync.set({[lenData+1]: users}, function () {
-    //         chrome.storage.sync.set({lenData: lenData+1});
-    //         console.log("user was added to sync");
-    //     })
-    // });
-
     chrome.storage.sync.set({users: users}, function (){
         console.log("user was saved to local")
     })
